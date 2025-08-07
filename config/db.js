11248f276977565e6235d4f3a9fd7ebd277daf2f
Unerbaev Imangali <gta.56.98@bk.ru>
2025-08-07 15:58:44 +0500

    rsedtgyjok

--- config/db.js
+++ config/db.js
@@ -4,7 +4,6 @@ const pool = new Pool({
   connectionString: 'postgresql://ims:EgsP9xwJlDytZJYafJnYmx7Dga4UAd7W@dpg-d2a6s3ur433s73a5m9og-a.oregon-postgres.render.com/finalproject_db_sl55',
   ssl: {rejectUnauthorized: false}
 });
-console.log(process.env.DATABASE_URL);
 
 
 module.exports = pool;
